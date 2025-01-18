25148f90d620e57331a8ef6d903297d51a23b677
Chew Boon Keat <jaychew.3753@gmail.com>
2025-01-18 15:11:19 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
+import { ValidationPipe, BadRequestException } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: (errors) => {
+        const messages = errors.map((err) => {
+          const constraints = Object.values(err.constraints).join(', ');
+          return `${constraints}`;
+        });
+
+        return new BadRequestException(
+          messages.length === 1 ? messages[0] : messages,
+        );
+      },
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
